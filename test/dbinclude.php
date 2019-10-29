f9dacc5f551cd356052af4bf30263eb6b3939a50
Sanyastiy <akselsv@mail.ru>
2019-10-30 00:24:02 +0300

    Revert "Started"
    This reverts commit 40e06c4342c33a602b77ec0b3a11d358ebdedd65.

--- test/dbinclude.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = mysqli_connect('localhost','cb03059_test','cb03059_test','cb03059_test');
-if(mysqli_connect_errno())
-{
-    echo"DB connection isn`t works((( with error number: ".mysqli_connect_errno()." :".mysqli_connect_error();
-    exit();
-}
